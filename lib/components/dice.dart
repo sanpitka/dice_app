cabc1e644b7fff7618fef5771f9938c4ca77d18c
sanpitka <sanpitka@student.oulu.fi>
2025-01-22 23:12:28 +0200

    Dice game 1.0: all buttons separated to widgets, roll button explained with image instead of Finnish text

--- lib/components/dice.dart
+++ lib/components/dice.dart
@@ -1,8 +1,8 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
 
-const Color buttonColor = Color.fromARGB(255, 241, 229, 190);
-const Color textColor = Color.fromARGB(255, 107, 58, 18);
+const Color diceAppYellow = Color.fromARGB(255, 241, 229, 190);
+const Color diceAppBrown = Color.fromARGB(255, 107, 58, 18);
 
 class Dice extends StatefulWidget {
   const Dice({super.key});
@@ -54,6 +54,141 @@ class _DiceState extends State<Dice> {
     });
   }
 
+  // Build buttons to increase or decrease the number of dice
+  Widget _buildDiceControlButtons() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 48.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: diceAppYellow,
+          borderRadius: BorderRadius.circular(30),
+        ),
+        padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            IconButton(
+              icon: const Icon(Icons.remove),
+              onPressed: () => _updateDiceCount(-1),
+            ),
+            IconButton(
+              icon: const Icon(Icons.add),
+              onPressed: () => _updateDiceCount(1),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // Build a button to clear all locked dice
+  Widget _buildClearSelectionButton() {
+    if (!_isLocked.contains(true)) return const SizedBox.shrink();
+
+    return Positioned(
+      top: 100,
+      left: 0,
+      right: 0,
+      child: Center(
+        child: ElevatedButton(
+          onPressed: _clearSelections,
+          style: ElevatedButton.styleFrom(
+            backgroundColor: diceAppYellow,
+            shape: const CircleBorder(),
+            minimumSize: const Size(60, 60),
+          ),
+          child: const Icon(
+            Icons.lock_open,
+            size: 30,
+            color: diceAppBrown,
+          ),
+        ),
+      ),
+    );
+  }
+
+  //Build dice images
+  Widget _buildDiceGrid() {
+    return Center(
+      child: ConstrainedBox(
+        constraints: const BoxConstraints(
+          maxWidth: 280,
+        ),
+        child: Wrap(
+          alignment: WrapAlignment.center,
+          spacing: 10.0,
+          runSpacing: 10.0,
+          children: List.generate(_rolls.length, (index) {
+            return GestureDetector(
+              onTap: () => _toggleLock(index),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(13),
+                  border: Border.all(
+                    color: _isLocked[index]
+                        ? Colors.red
+                        : Colors.transparent,
+                    width: 3,
+                  ),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.asset(
+                    'assets/images/dice${_rolls[index]}.png',
+                    fit: BoxFit.contain,
+                    width: 80,
+                    height: 80,
+                  ),
+                ),
+              ),
+            );
+          }),
+        ),
+      ),
+    );
+  }
+
+  // Build a button to roll the dice
+  Widget _buildRollDiceButton() {
+    return ElevatedButton(
+      onPressed: _diceroll,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: diceAppYellow,
+        foregroundColor: diceAppBrown,
+        minimumSize: const Size(100, 50),
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Transform.rotate(
+            angle: -pi / 6,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(5), // Slightly curve the die image edges
+              child: Image.asset(
+                'assets/images/dice2.png',
+                width: 30,
+                height: 30,
+              ),
+            ),
+          ),
+          const SizedBox(width: 15),
+          Transform.rotate(
+            angle: pi / 7,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(5), // Slightly curve the die image edges
+              child: Image.asset(
+                'assets/images/dice5.png',
+                width: 30,
+                height: 30,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -61,108 +196,15 @@ class _DiceState extends State<Dice> {
         children: [
           Column(
             children: [
-              // Buttons for adding and removing dice
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 48.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: buttonColor,
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                  padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      IconButton(
-                        icon: const Icon(Icons.remove),
-                        onPressed: () => _updateDiceCount(-1),
-                      ),
-                      IconButton(
-                        icon: const Icon(Icons.add),
-                        onPressed: () => _updateDiceCount(1),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              // Room for dice images
-              Expanded(
-                child: Center(
-                  child: ConstrainedBox(
-                    constraints: const BoxConstraints(
-                      maxWidth: 280,
-                    ),
-                    child: Wrap(
-                      alignment: WrapAlignment.center,
-                      spacing: 10.0,
-                      runSpacing: 10.0,
-                      children: List.generate(_rolls.length, (index) {
-                        return GestureDetector(
-                          onTap: () => _toggleLock(index), // Lock or unlock the die when tapped
-                          child: Container(
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(13),
-                              border: Border.all(
-                                color: _isLocked[index]
-                                    ? Colors.red // Red border for locked dice
-                                    : Colors.transparent, // No border for unlocked dice
-                                width: 3,
-                              ),
-                            ),
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(10),
-                              child: Image.asset(
-                                'assets/images/dice${_rolls[index]}.png',
-                                fit: BoxFit.contain,
-                                width: 80,
-                                height: 80,
-                              ),
-                            ),
-                          ),
-                        );
-                      }),
-                    ),
-                  ),
-                ),
-              ),
-              // Roll dice button
+              _buildDiceControlButtons(),
+              Expanded(child: _buildDiceGrid()),
               Padding(
                 padding: const EdgeInsets.symmetric(vertical: 32.0),
-                child: ElevatedButton(
-                  onPressed: _diceroll,
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: buttonColor,
-                    foregroundColor: textColor,
-                    minimumSize: const Size(100, 50),
-                  ),
-                  child: const Text('Heitä noppaa', style: TextStyle(fontSize: 24)),
-                ),
+                child: _buildRollDiceButton(),
               ),
             ],
           ),
-          // Clear selections button with lock icon
-          if (_isLocked.contains(true))
-            Positioned(
-              top: 120,
-              left: 0,
-              right: 0,
-              child: Center(
-                child: ElevatedButton(
-                  onPressed: _clearSelections,
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: buttonColor,
-                    shape: const CircleBorder(),
-                    minimumSize: const Size(60, 60),
-                  ),
-                  child: const Icon(
-                    Icons.lock_open, 
-                    size: 30, 
-                    color: textColor,
-                  ), // Lock open icon
-                ),
-              ),
-            ),
+          _buildClearSelectionButton(),
         ],
       ),
     );
